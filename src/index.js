f57fb004e359c430753a907b75f115c864a357a6
wanguohui <wan00038@algonquinlive.com>
2024-02-01 09:50:19 -0500

    update dependency

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
 import { store } from './app/store';
 import { Provider } from 'react-redux';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 
+if(process.env.NODE_ENV === 'production') disableReactDevTools()
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
